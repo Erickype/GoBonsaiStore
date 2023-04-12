2b227a6cb141feab014e0e3c76c88aa62138b1a7
Erickype <67160282+Erickype@users.noreply.github.com>
2023-04-12 11:38:41 -0500

    Product struct

---
+++ api/product/structs.go
@@ -0,0 +1,8 @@
+package product
+
+type Product struct {
+	Id                int    `json:"id"`
+	CategoryId        int    `json:"categoryId"`
+	Name              string `json:"name"`
+	ManufacturingDate string `json:"manufacturingDate"`
+}
