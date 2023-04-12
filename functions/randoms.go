198a78e685d740829c3d98a20a9efac921746f11
Erickype <67160282+Erickype@users.noreply.github.com>
2023-04-12 15:38:23 -0500

    Generate suppliers handler function

--- functions/randoms.go
+++ functions/randoms.go
@@ -1,6 +1,11 @@
 package functions
 
-import "math/rand"
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"math/rand"
+	"time"
+)
 
 func NewRandomValue(upper int) int {
 	randomValue := rand.Intn(upper)
@@ -10,4 +15,19 @@ func NewRandomValue(upper int) int {
 func RandomIndexValue(vector []int) int {
 	i := rand.Intn(len(vector))
 	return vector[i]
+}
+
+func RandomPhoneWithId(id int) string {
+	rand.NewSource(time.Now().UnixNano())
+	phoneNumber := string(rune(id))
+
+	for i := 0; i < 9; i++ {
+		digit := rand.Intn(10)
+		phoneNumber += fmt.Sprintf("%d", digit)
+	}
+	return phoneNumber
+}
+
+func RandomAddress() string {
+	return uuid.New().String()
 }
