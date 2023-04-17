5d6c1055c851aa429df19e1310cdfbe97eb8f295
Erickype <67160282+Erickype@users.noreply.github.com>
2023-04-17 17:30:40 -0500

    Purchase detail struct

--- api/purchase/structs.go
+++ api/purchase/structs.go
@@ -6,3 +6,12 @@ type Purchase struct {
 	Date       string `json:"date"`
 	Cancel     bool   `json:"cancel"`
 }
+
+type Detail struct {
+	Id              int     `json:"id"`
+	ProductId       int     `json:"productId"`
+	PurchaseId      int     `json:"purchaseId"`
+	SupplierId      int     `json:"supplierId"`
+	ProductQuantity int     `json:"productQuantity"`
+	ProductPrice    float64 `json:"productPrice"`
+}
