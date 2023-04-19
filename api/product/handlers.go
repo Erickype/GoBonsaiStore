8f5562aeba052f33e36c799a79833237f7eb3c17
Erickype <67160282+Erickype@users.noreply.github.com>
2023-04-19 12:25:06 -0500

    Handlers to generate products based on the rest of the categories excluding Bonsai

--- api/product/handlers.go
+++ api/product/handlers.go
@@ -26,8 +26,64 @@ func GenerateBonsaiProducts(c *gin.Context, db *sql.DB) {
 	c.JSON(http.StatusOK, response)
 }
 
+func GenerateSoilProducts(c *gin.Context, db *sql.DB) {
+	soilCategories := getCategoriesByParentId(db, 12)
+	nProducts := functions.NewRandomValue(500)
+	query := "insert into producto (categoria_id, nombre, fecha_fabricacion)  values ($1, $2,$3)"
+	for i := 0; i < nProducts; i++ {
+		name := functions.RandomFancyName()
+		catId := functions.RandomIndexValue(soilCategories)
+		date := functions.GenerateDateInRange(2020, 2022)
+		_, err := db.Exec(query, catId, name, date)
+		if err != nil {
+			panic(err)
+		}
+	}
+	response := structs.Response{Status: "Soil products generated"}
+	c.JSON(http.StatusOK, response)
+}
+
+func GenerateToolsProducts(c *gin.Context, db *sql.DB) {
+	soilCategories := getCategoriesByParentId(db, 13)
+	nProducts := functions.NewRandomValue(500)
+	query := "insert into producto (categoria_id, nombre, fecha_fabricacion)  values ($1, $2,$3)"
+	for i := 0; i < nProducts; i++ {
+		name := functions.RandomFancyName()
+		catId := functions.RandomIndexValue(soilCategories)
+		date := functions.GenerateDateInRange(2020, 2022)
+		_, err := db.Exec(query, catId, name, date)
+		if err != nil {
+			panic(err)
+		}
+	}
+	response := structs.Response{Status: "Tools products generated"}
+	c.JSON(http.StatusOK, response)
+}
+
+func GeneratePotsProducts(c *gin.Context, db *sql.DB) {
+	soilCategories := getCategoriesByParentId(db, 14)
+	nProducts := functions.NewRandomValue(500)
+	query := "insert into producto (categoria_id, nombre, fecha_fabricacion)  values ($1, $2,$3)"
+	for i := 0; i < nProducts; i++ {
+		name := functions.RandomFancyName()
+		var date string
+		catId := functions.RandomIndexValue(soilCategories)
+		if catId == 33 {
+			date = functions.GenerateDateInRange(1980, 2000)
+		} else {
+			date = functions.GenerateDateInRange(2020, 2022)
+		}
+		_, err := db.Exec(query, catId, name, date)
+		if err != nil {
+			panic(err)
+		}
+	}
+	response := structs.Response{Status: "Pots products generated"}
+	c.JSON(http.StatusOK, response)
+}
+
 func DeleteBonsaiProducts(c *gin.Context, db *sql.DB) {
-	_, err := db.Exec("delete from producto")
+	_, err := db.Exec("delete from producto as P\nwhere P.categoria_id in (select Sub.categoria_id\n                     from categoría as Sub\n                     where Sub.categoria_padre = (select Cat.categoria_id\n                                                  from categoría as Cat\n                                                  where Cat.nombre = 'Bonsai'));\n")
 	if err != nil {
 		panic(err)
 	}
--- api/product/routes.go
+++ api/product/routes.go
@@ -12,4 +12,13 @@ func Routes(router *gin.Engine, db *sql.DB) {
 	router.GET("/products/bonsai/delete", func(context *gin.Context) {
 		DeleteBonsaiProducts(context, db)
 	})
+	router.GET("/products/soil/generate", func(context *gin.Context) {
+		GenerateSoilProducts(context, db)
+	})
+	router.GET("/products/tools/generate", func(context *gin.Context) {
+		GenerateToolsProducts(context, db)
+	})
+	router.GET("/products/pots/generate", func(context *gin.Context) {
+		GeneratePotsProducts(context, db)
+	})
 }
