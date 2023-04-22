ebaff2d1e414823fd9dd2d96afb03ab963b38699
Erickype <67160282+Erickype@users.noreply.github.com>
2023-04-22 11:14:23 -0500

    Generate sales handler implemented

--- api/sale/handlers.go
+++ api/sale/handlers.go
@@ -1,6 +1,7 @@
 package sale
 
 import (
+	"BonsaiStore/api/client"
 	"BonsaiStore/functions"
 	"BonsaiStore/structs"
 	"database/sql"
@@ -9,8 +10,20 @@ import (
 	"net/http"
 )
 
-func GenerateSales(c *gin.Context, _ *sql.DB) {
-	nPurchases := functions.NewRandomValue(5000)
+func GenerateSales(c *gin.Context, db *sql.DB) {
+	query := "insert into venta (cliente_id, fecha, cancelado) values($1,$2,$3)"
+	nPurchases := functions.NewRandomValue(4000)
+	clientIds := client.GetIds(db)
+
+	for i := 0; i < nPurchases; i++ {
+		clientId := functions.RandomIndexValue(clientIds)
+		saleDate := functions.GenerateDateInRange(2001, 2023)
+
+		_, err := db.Exec(query, clientId, saleDate, false)
+		if err != nil {
+			panic(err)
+		}
+	}
 
 	message := fmt.Sprintf("Sales generated: %d", nPurchases)
 	response := structs.Response{Status: message}
