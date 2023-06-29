902ce90dcdd49b2b7c89686e19100ecf6811e1e9
Erickype <erickype@hotmail.com>
2023-06-29 13:19:26 -0500

    Bonsai and pots standard measures

--- api/product/constants.go
+++ api/product/constants.go
@@ -6,3 +6,26 @@ var prices = map[int]float64{
 	13: 8.75,
 	14: 5.99,
 }
+
+var measures = map[int]map[int][]float64{
+	1: { //h,  w,  d
+		2:  []float64{30, 25, 20}, // Moyogi
+		3:  []float64{35, 30, 25}, // Fukinagashi
+		4:  []float64{40, 35, 30}, // Kabudachi
+		5:  []float64{25, 20, 15}, // Literati
+		6:  []float64{35, 25, 20}, // Shakan
+		7:  []float64{30, 30, 30}, // Ikadabuki
+		8:  []float64{45, 40, 35}, // Han-Kengai
+		9:  []float64{40, 30, 25}, // Yose-ue
+		10: []float64{35, 30, 25}, // Neagari
+		11: []float64{30, 25, 20}, // Ishitsuki
+	},
+	14: {
+		33: []float64{20, 15, 15}, // Macetas de cerámica
+		34: []float64{25, 20, 20}, // Macetas de madera
+		35: []float64{15, 12, 12}, // Macetas de arcilla
+		36: []float64{18, 16, 14}, // Macetas de plástico
+		37: []float64{22, 18, 18}, // Macetas de vidrio
+		38: []float64{20, 20, 20}, // Macetas de metal
+	},
+}
